cd4dd08b4a3554dc2af1a9a66a2b0b1852f5700c
Nathan Wilson <Nathan@Xeppit.co.uk>
2020-07-12 13:51:59 +0100

    Config IdentityServer

---
+++ MicroserviceArchitecture.IdentityServer/CustomIdentityResourcesProfile.cs
@@ -0,0 +1,13 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+
+namespace MicroserviceArchitecture.IdentityServer
+{
+    public class CustomIdentityResourcesProfile : IdentityResources.Profile
+    {
+        public CustomIdentityResourcesProfile()
+        {
+            this.UserClaims.Add(JwtClaimTypes.Role);
+        }
+    }
+}
--- MicroserviceArchitecture.IdentityServer/SeedData.cs
+++ MicroserviceArchitecture.IdentityServer/SeedData.cs
@@ -56,6 +56,7 @@ namespace MicroserviceArchitecture.IdentityServer
                             new Claim(JwtClaimTypes.GivenName, "Alice"),
                             new Claim(JwtClaimTypes.FamilyName, "Smith"),
                             new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim(JwtClaimTypes.Role, "Admin") // New Line
                         }).Result;
                         if (!result.Succeeded)
                         {
